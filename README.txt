f40f55e19d9306f80fdb477ccbaedfb19743ee81
Danielle DuChene <67253325+aceDuChene@users.noreply.github.com>
2022-03-17 17:10:57 -0700

    add and adjust testscript
    testscript and binary file to run

--- README.txt
+++ README.txt
@@ -1,9 +1,10 @@
 To compile, type into the linux terminal
-gcc -g -Wall -std=gnu99 main.c -o smallsh
+gcc -g -std=gnu99 main.c -o smallsh
 
-then to run, type in the terminal
+Then to run, type in the terminal
 ./smallsh
 
-I am aware of at least one error that shows up in wall,
-"function returns address of local variable" so hopefully
-I can fix that before the due date. Thanks!
+To run the test script, you may have to 
+change the permissions, then run it:
+chmod +x ./testscript
+./testscript 2>&1
--- main.c
+++ main.c
@@ -6,13 +6,15 @@
  * @editedOn    : 3/17/2022
  * @description : This is smallsh, an assignment done for CS344
  *   Operating Systems I for Oregon State University Ecampus.
+ *   This specific implementation has been edited down from
+ *   the original requirements.
  *   A shell that implements a subset of features of well-known
  *   shells. As listed on the assignment description, it will
  *      1. Provide a prompt for running commands
  *      2. Handle blank lines and comments (beginning with #)
  *      3. Execute exit, cd, and status via code built into shell
  *      4. Execute other commands by creating new processes using
- *          a function from the exec family of functions.
+ *          a function from the exec family of functions
  *      5. Support input and output redirection
  *      6. Can run foreground commands & background processes
  *      7. Implement custom handlers for 2 signals SIGINT & SIGTSTP
